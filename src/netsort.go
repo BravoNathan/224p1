b5a82363ac948c7e24d985e24c0543d66a2ddb12
Zhengyang <zyli1029@gmail.com>
2023-01-26 12:57:41 -0800

    proj1_2

--- src/netsort.go
+++ src/netsort.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -11,6 +12,7 @@ import (
 	"sort"
 	"strconv"
 	"sync"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
@@ -22,6 +24,13 @@ type ServerConfigs struct {
 		Port     string `yaml:"port"`
 	} `yaml:"servers"`
 }
+
+type KVpair struct {
+	Key   []byte
+	Value []byte
+}
+
+var receivedList []KVpair
 
 func readServerConfigs(configPath string) ServerConfigs {
 	f, err := ioutil.ReadFile(configPath)
@@ -65,10 +74,6 @@ func main() {
 	var inputfile string = os.Args[2]
 	var outputfile string = os.Args[3]
 
-	// Create a bi-directional channel
-	bidirectional_ch := make(chan []byte)
-	defer close(bidirectional_ch)
-
 	/*
 	 * Spawn a new goroutine that concurrently listens for connections from
 	 * clients and pass the send side of this channel to this go-routine.
@@ -78,11 +83,27 @@ func main() {
 	service := scs.Servers[serverId].Host + ":" + scs.Servers[serverId].Port
 	fmt.Printf("Now server is %s \n ", service)
 
+	// create map to store arranged data before sending
+	map_data := make(map[int][]KVpair)
+	server_bits_number := PowerOfTwo(len(scs.Servers))
+	fmt.Printf("server_bits_number: %d \n", server_bits_number)
+	arrangingLocalData(inputfile, map_data, server_bits_number)
+
 	listener, err := net.Listen("tcp", service)
 	checkError(err)
+	defer listener.Close()
 
+	var mutex sync.Mutex
 	var wg_server sync.WaitGroup
-	wg_server.Add(len(scs.Servers) - 1)
+	wg_server.Add(len(scs.Servers))
+
+	fmt.Println("Before Wait")
+	go func() {
+
+		defer wg_server.Done()
+
+		listeningConn(listener, mutex, scs)
+	}()
 
 	for i := 0; i < len(scs.Servers); i++ {
 		if i == serverId {
@@ -95,108 +116,27 @@ func main() {
 		go func() {
 			defer wg_server.Done()
 
-			var conn net.Conn
-			for {
-				conn1, err := net.Dial("tcp", other_server)
-				if err == nil {
-					conn = conn1
-					break
-				}
+			conn, err := net.Dial("tcp", other_server)
+			for err != nil {
+				time.Sleep(time.Duration(50) * time.Microsecond)
+				conn, err = net.Dial("tcp", other_server)
+				checkError(err)
 			}
 			defer conn.Close()
-
-			fmt.Printf("Server %d Connected to Server %d \n", serverId, other_server_id)
-			server_number := len(scs.Servers)
-			// fmt.Printf("server_number: %d \n", server_number)
-			bytes_for_server := PowerOfTwo(server_number)
-			fmt.Printf("bytes_for_server: %d \n", bytes_for_server)
-
-			f, err := os.Open(inputfile)
-			checkError(err)
-			defer f.Close()
-
-			for {
-				// read key-value pair from the file
-				client_msg_buf := make([]byte, 100)
-				bytes_read, err := f.Read(client_msg_buf)
-				if err != nil && err != io.EOF {
-					log.Fatal(err)
-				}
-
-				if err == io.EOF {
-					break
-				}
-				/*
-					get the first "bytes_for_server" bytes and if it matches the current client_id,
-					write to the conn
-				*/
-
-				fmt.Println("Bug_1")
-
-				client_number_byte := make([]byte, bytes_for_server)
-
-				// fmt.Printf("Length of client_msg_buf %d \n", len(client_msg_buf))
-
-				copy(client_number_byte, client_msg_buf[:bytes_for_server])
-
-				// fmt.Printf("Length of client_msg_buf[:bytes_for_server] %d \n", len(client_msg_buf[:bytes_for_server]))
-
-				// fmt.Printf("Length of client_number_byte %d \n", len(client_number_byte))
-
-				// fmt.Println("Bug_2")
-				fmt.Printf("client_number_byte: %b", client_number_byte)
-				client_number := extractBits(client_msg_buf, bytes_for_server)
-				fmt.Printf("client_number: %d \n", client_number)
-
-				if client_number == other_server_id {
-					fmt.Printf("Writing key-value to %d conn \n", client_number)
-					stream_complete := []byte{0}
-					client_msg := client_msg_buf[0:bytes_read]
-					message := append(stream_complete, client_msg...)
-					fmt.Printf("Length of message: %d \n", len(message))
-					conn.Write(message)
-				}
-			}
-			end_byte_stream := make([]byte, 100)
-			stream_complete := []byte{1}
-			message := append(stream_complete, end_byte_stream...)
-			conn.Write(message)
+			fmt.Printf("Length of map: %d \n", len(map_data[other_server_id]))
+			sendingData(conn, map_data[other_server_id])
 		}()
 	}
 	wg_server.Wait()
+	fmt.Printf("After Wait, %d \n", len(receivedList))
 
-	var client_message [][]byte
-	var mutex sync.Mutex
-
-	var wg sync.WaitGroup
-	wg.Add(len(scs.Servers) - 1)
-	for i := 0; i < len(scs.Servers)-1; i++ {
-		go func() {
-			defer wg.Done()
-			conn, err := listener.Accept()
-			checkError(err)
-			defer conn.Close()
-
-			var message_receive [][]byte
-			bytes_read := make([]byte, 101)
-			for {
-				n, err := conn.Read(bytes_read)
-				checkError(err)
-				if bytes_read[0] == byte(1) {
-					break
-				}
-				message_receive = append(message_receive, bytes_read[:n])
-			}
-			mutex.Lock()
-			client_message = append(client_message, message_receive...)
-			mutex.Unlock()
-		}()
+	for _, message := range map_data[serverId] {
+		receivedList = append(receivedList, message)
 	}
-	wg.Wait()
-
+	fmt.Printf("--------------Length of receivedList: %d \n", len(receivedList))
 	//process the message we got
-	sortTheMessages(client_message, outputfile)
-
+	sortTheMessages(receivedList, outputfile)
+	fmt.Println("chai zhiyuan")
 }
 
 func checkError(err error) {
@@ -205,137 +145,96 @@ func checkError(err error) {
 	}
 }
 
-// func listenForClientConnections(write_only chan<- []byte, listener net.Listener, scs ServerConfigs, inputfile string) {
+func arrangingLocalData(inputfile string, map_data map[int][]KVpair, server_bits int) {
+	f, err := os.Open(inputfile)
+	checkError(err)
+	defer f.Close()
 
-// 	defer listener.Close()
+	for {
+		msg_buf := make([]byte, 100)
+		_, err := f.Read(msg_buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			checkError(err)
+		}
+		client_number := extractBits(msg_buf, server_bits)
+		var kv KVpair
+		kv.Key = append(kv.Key, msg_buf[:10]...)
+		kv.Value = append(kv.Value, msg_buf[10:100]...)
+		// fmt.Printf("Arrange: client_number: %d, Key: %s \n", client_number, hex.EncodeToString(kv.Key))
+		map_data[client_number] = append(map_data[client_number], kv)
+		// fmt.Printf("After appending, Server %s - %d length of map_data: %d \n", os.Args[1], client_number, len(map_data[client_number]))
+	}
 
-// 	for {
-// 		conn, err := listener.Accept()
-// 		if err != nil {
-// 			continue
-// 		}
-// 		fmt.Println("Client: " + conn.RemoteAddr().String() + " connected, spawning goroutine to handle connection")
-// 		// Spawn a new goroutine to handle this client's connections
-// 		// and go back to listening for additional connections
-// 		go handleClientConnection(conn, write_only, inputfile, scs)
+}
 
-// 	}
+func listeningConn(listener net.Listener, mutex sync.Mutex, scs ServerConfigs) {
+	for i := 0; i < len(scs.Servers)-1; i++ {
+		conn, err := listener.Accept()
+		if err != nil {
+			// server closed
+			break
+		}
+		receivingData(conn, mutex)
+	}
+}
 
-// }
+func writeNBytes(connection net.Conn, buffer []byte, length int) {
+	cnt, _ := connection.Write(buffer)
+	for cnt < length {
+		n, _ := connection.Write(buffer[cnt:])
+		cnt += n
+	}
+}
 
-// func handleClientConnection(conn net.Conn, write_only_ch chan<- []byte, inputfile string, scs ServerConfigs) {
-// 	defer conn.Close()
-// 	fmt.Println("Step into handleClientConnection func _ 1")
-// 	//get first bits to confirm which data would be sent to this server(client).
-// 	server_number := len(scs.Servers)
-// 	fmt.Printf("server_number: %d \n", server_number)
+func readNBytes(connection net.Conn, buffer []byte, length int) {
+	cnt, _ := connection.Read(buffer[0:])
+	for cnt < length {
+		n, err := connection.Read(buffer[cnt:])
+		if err != nil && err != io.EOF {
+			break
+		}
+		cnt += n
+	}
+}
 
-// 	bytes_for_server := PowerOfTwo(server_number)
-// 	fmt.Printf("bytes_for_server: %d \n", bytes_for_server)
+func sendingData(conn net.Conn, messages []KVpair) {
+	for _, message := range messages {
+		send_buf := make([]byte, 1)
+		send_buf[0] = byte(0)
+		send_buf = append(send_buf, message.Key...)
+		send_buf = append(send_buf, message.Value...)
+		// fmt.Printf("client_number: %s, sending Key: %s \n", os.Args[1], hex.EncodeToString(message.Key))
+		writeNBytes(conn, send_buf, 101)
+	}
+	send_end := make([]byte, 1)
+	send_end[0] = byte(1)
+	writeNBytes(conn, send_end, 1)
+}
 
-// 	// get the specific client_id by port number
-// 	client_socket_string := conn.RemoteAddr().String()
-// 	client_port := strings.Split(client_socket_string, ":")[1]
-// 	fmt.Printf("Client port is %s \n", client_port)
-// 	var client_id int
-// 	for i := 0; i < server_number; i++ {
-// 		if client_port == scs.Servers[i].Port {
-// 			client_id = scs.Servers[i].ServerId
-// 		}
-// 	}
+func receivingData(conn net.Conn, mutex sync.Mutex) {
+	isValid := make([]byte, 1)
+	message := make([]byte, 100)
+	conn.Read(isValid)
 
-// 	f, err := os.Open(inputfile)
-// 	checkError(err)
-// 	defer f.Close()
+	for isValid[0] == byte(0) {
+		readNBytes(conn, message, 100)
 
-// 	for {
-// 		// read key-value pair from the file
-// 		client_msg_buf := make([]byte, 100)
-// 		bytes_read, err := f.Read(client_msg_buf)
-// 		if err != nil && err != io.EOF {
-// 			log.Fatal(err)
-// 		}
+		var kvpair KVpair
+		kvpair.Key = append(kvpair.Key, message[:10]...)
+		kvpair.Value = append(kvpair.Value, message[10:100]...)
+		fmt.Printf("Client number: %s, received: %s \n", os.Args[1], hex.EncodeToString(kvpair.Key))
 
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		/*
-// 			get the first "bytes_for_server" bytes and if it matches the current client_id,
-// 			write to the channel
-// 		*/
-// 		client_number_byte := make([]byte, bytes_for_server)
-// 		copy(client_number_byte, client_msg_buf[:bytes_for_server])
-// 		client_number := int(binary.BigEndian.Uint32(client_number_byte))
-// 		fmt.Println("Bug_6")
-// 		fmt.Printf("Client number is : %d \n", client_number)
-// 		if client_number == client_id {
-// 			fmt.Printf("Writing key-value to %d channel \n", client_id)
-// 			stream_complete := []byte{0}
-// 			client_msg := client_msg_buf[0:bytes_read]
-// 			message := append(stream_complete, client_msg...)
-// 			write_only_ch <- message
-// 		}
+		mutex.Lock()
+		receivedList = append(receivedList, kvpair)
+		fmt.Printf("After receiving, length of receivedList: %d \n", len(receivedList))
+		mutex.Unlock()
+		conn.Read(isValid)
+	}
 
-// 	}
-// 	write_only_ch <- []byte{1}
-// }
-
-// func clientRequestConnection(read_only_ch <-chan []byte, scs ServerConfigs, serverId int) [][]byte {
-// 	var message_receive [][]byte
-// 	for i := serverId + 1; i < len(scs.Servers); i++ {
-// 		server_addr := scs.Servers[i].Host + ":" + scs.Servers[i].Port
-// 		fmt.Printf("Client %d ready to request to connect Server %d \n", serverId, scs.Servers[i].ServerId)
-// 		var conn net.Conn
-// 		for {
-// 			conn_t, err := net.Dial("tcp", server_addr)
-
-// 			if err == nil {
-// 				fmt.Printf("Client %d connected Server %d Successfully \n", scs.Servers[i].ServerId, serverId)
-// 				break
-// 			}
-// 			conn = conn_t
-// 		}
-// 		defer conn.Close()
-
-// 		var client_message [][]byte
-// 		for {
-// 			message := <-read_only_ch
-// 			if message[0] == byte(1) {
-// 				break
-// 			}
-// 			client_message = append(client_message, message)
-// 		}
-
-// 		message_receive = append(message_receive, client_message...)
-
-// 		// if err != nil {
-// 		// 	log.Fatalln("Client" + strconv.Itoa(serverId) + " connected to Server" + server_addr + "Failed")
-// 		// }
-// 		// only when we get the client_message from all the server, can we exit the main func
-// 		// else we need to wait for the messages.
-// 		// client_message = append(client_message, receiveDataFromServers(bidirectional_ch)...)
-
-// 	}
-// 	return message_receive
-// }
-
-// func receiveDataFromServers(read_only_ch <-chan []byte) [][]byte {
-// 	/*
-// 	 * the main goroutine waits for the completion of all other goroutines it had spawned
-// 	 * before exiting.
-// 	 */
-
-// 	//only message from specific server do we handle
-// 	var client_message [][]byte
-// 	for {
-// 		message := <-read_only_ch
-// 		if message[0] == byte(1) {
-// 			break
-// 		}
-// 		client_message = append(client_message, message)
-// 	}
-// 	return client_message
-// }
+}
 
 func PowerOfTwo(num int) int {
 	count := 0
@@ -350,9 +249,9 @@ func PowerOfTwo(num int) int {
 	return count
 }
 
-func sortTheMessages(message [][]byte, outputfile string) {
+func sortTheMessages(message []KVpair, outputfile string) {
 
-	sort.Slice(message, func(i, j int) bool { return bytes.Compare(message[i][:11], message[j][:11]) < 0 })
+	sort.Slice(message, func(i, j int) bool { return bytes.Compare(message[i].Key, message[j].Key) < 0 })
 
 	f, err := os.Create(outputfile)
 	if err != nil {
@@ -361,20 +260,18 @@ func sortTheMessages(message [][]byte, outputfile string) {
 	defer f.Close()
 
 	for _, element := range message {
-		f.Write(element)
+		_, err := f.Write(element.Key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = f.Write(element.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func extractBits(buf []byte, numBits int) int {
-	// Get the byte that contains the bits
-	byteNum := numBits / 8
-	// Get the position of the bits in the byte
-	bitPos := numBits % 8
-
-	// Create a mask to extract the bits
-	mask := byte(1<<uint(bitPos)) - 1
-	// Shift the byte to the right to discard the unneeded bits
-	result := int(buf[byteNum]) & int(mask)
-
+	result := int(buf[0] >> (8 - numBits))
 	return result
 }
